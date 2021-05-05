9159306fa8590bc50b6437868a6a3b9a715930de
ikrums <67041088+ikrums@users.noreply.github.com>
2021-05-05 20:17:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,7 +48,6 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
 """
 
 
